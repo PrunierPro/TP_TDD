a1e2d235f0683a44d7f32d89a6278109f013d94d
Thomas Prunier <thomas.prunier@labom2iformation.fr>
2023-08-25 14:15:46 +0200

    Implement list of villes and find by beginning functionality

--- TP_TDDLibrary/RechercheVille.cs
+++ TP_TDDLibrary/RechercheVille.cs
@@ -4,12 +4,15 @@ namespace TP_TDDLibrary
 {
     public class RechercheVille
     {
-        private List<String> villes;
+        private List<String> villes = new List<String>(new String[] {"Valenciennes", "Vancouver"});
 
         public List<String> Rechercher(String mot)
         {
             if (mot.Length < 2) throw new NotFoundException();
-            else throw new NotImplementedException();
+            else
+            {
+                return villes.FindAll(s => s.StartsWith(mot));
+            }
         }
 
     }
