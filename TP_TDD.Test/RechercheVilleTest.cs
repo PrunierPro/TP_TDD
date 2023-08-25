f25b559d6fd702419750a715060e5a44c3c9e529
Thomas Prunier <thomas.prunier@labom2iformation.fr>
2023-08-25 13:47:43 +0200

    Test "WhenMotLength_LessThan2_Then_NotFoundException" implemented

--- TP_TDD.Test/RechercheVilleTest.cs
+++ TP_TDD.Test/RechercheVilleTest.cs
@@ -1,11 +1,16 @@
+using TP_TDDLibrary;
+
 namespace TP_TDD.Test
 {
     [TestClass]
     public class RechercheVilleTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void WhenMotLength_LessThan2_Then_NotFoundException()
         {
+            RechercheVille rechercheVille = new RechercheVille();
+            string query = "hi";
+            Assert.ThrowsException<NotFoundException>(() => rechercheVille.Rechercher(query));
         }
     }
 }
